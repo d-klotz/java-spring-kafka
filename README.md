20bd6620f88798e1b6301754d5641f8cf6c16d15
d-klotz <danielfelipeklotz@gmail.com>
2020-06-11 12:55:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+<h1 align="center">Kafka producer/consumer application</h1>
+
+## :electric_plug: Requirements
+
+- Java 8
+- Maven
+- Kafka
+
+# :closed_lock_with_key: Initital Instructions
+### Installing Kafka
+In order to run the project, you will need to have Kafka and Kafka Zookeeper running.
+
+```shell
+# In Kafka root directory run the command below to start kafka
+$ kafka-server-start.bat config\server.properties
+```
+
+```shell
+# In Kafka root directory run the command below to start Zookeeper
+$ zookeeper-server-start.bat config\zookeeper.properties
+```
+
+Clone this repository and install all dependencies.
+
+```shell
+# Install all dependencies using Maven
+$ mvn install
+```
+
+<hr />
+
+### <a href="http://linkedin.com/in/danielfelipeklotz">Contact me on LinkedIn</a>
---
+++ src/main/java/com/klotz/kafka/tutorial1/ProducerDemo.java
@@ -0,0 +1,31 @@
+package com.klotz.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("first_topic", "hello World");
+        producer.send(record);
+
+        //kafka produces messages asynchronously, thus if we want to send the message before
+        //the application terminates, we need to use the flush method, this will force kafka
+        //to send all messages
+        producer.flush();
+
+        //closes the producer;
+        producer.close();
+    }
+}
